e943add6023f8acb983b1ee5d56654275e2de196
liunanchenFYJJ <liunanchen@163.com>
2017-11-03 19:03:54 +0800

    footer添加

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,67 @@
 <template>
   <div id="app">
-    <router-link to="/foo">to foo</router-link>
-    <router-link to="/bar">to bar</router-link>
-    <router-link to="/">back</router-link>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li id="aa"><router-link to="/foo">to foo</router-link></li>
+            <li><router-link to="/bar">to bar</router-link></li>
+            <li><router-link to="/">back</router-link></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Default</a></li>
+            <li><a href="../navbar-static-top/">Static top</a></li>
+            <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+
     <router-link to="/hello">to hello</router-link>
     <router-view/>
+
+    <footer class="footer">
+      <div class="container">
+        <p>footer here</p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
 
@@ -22,5 +73,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #CC9966;
+}
+#aa:active{
+  background-color: red
+}
+.footer > .container {
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-bottom: 20px
 }
 </style>
--- src/components/bar.vue
+++ src/components/bar.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="bar">
-    <h4>bar here</h4>
+    <h4 style="marginTop:100px">bar here</h4>
+    <hr>
+    {{parserInt(a)+b}}
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'bar',
+    data () {
+      return {
+        a: '1',
+        b: 4
+      }
+    }
+  }
+</script>
